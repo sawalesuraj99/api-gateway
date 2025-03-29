4c59b7fe12780a76f3accaea698af37a2de4c3a5
Suraj sawale <sawalesuraj99@gmail.com>
2025-03-29 19:43:55 +0530

    api gaterway project

---
+++ src/main/java/com/apigateway/app/ApiGateWayApplication.java
@@ -0,0 +1,25 @@
+package com.apigateway.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateWayApplication.class, args);
+	}
+	
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("PRODUCTSERVICE", r -> r.path("/product/api/v1/**")
+						.uri("lb://PRODUCTSERVICE"))
+				.build();
+	}
+}
